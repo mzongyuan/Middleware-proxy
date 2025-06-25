1738dc1c26ab95555edf40c3850c601c8aaea0e2
mzongyuan <1414763271@qq.com>
2025-06-25 20:32:18 +0800

    Project init

---
+++ src/test/java/com/example/middlewareproxy/MiddlewareProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.middlewareproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
